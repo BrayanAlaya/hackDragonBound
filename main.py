606ee11103169e5da85b87100315b38601602982
brayan anthony alaya atalaya <brayan.alaya@hotmail.com>
2024-07-18 15:36:49 -0500

    nada que ver

--- main.py
+++ main.py
@@ -2,13 +2,6 @@ from PIL import Image
 import math
 from pynput import mouse
 
-
-
-def calcular_distancia(punto1, punto2):
-    x1, y1 = punto1
-    x2, y2 = punto2
-    return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
-
 clics = []
 
 def on_click(x, y, button, pressed):
@@ -30,6 +23,11 @@ with mouse.Listener(on_click=on_click) as listener:
     listener.join()
 
     
+def calcular_distancia(punto1, punto2):
+    x1, y1 = punto1
+    x2, y2 = punto2
+    return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
 # Calcula la distancia
 distancia = calcular_distancia(clics[0], clics[1])
 print(f"La distancia entre los puntos es: {distancia} píxeles")
